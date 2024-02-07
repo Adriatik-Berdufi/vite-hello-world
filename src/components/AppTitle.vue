3c1d0a4deaaf5ad9f22fceb9c20d54303cd1e539
Adriatik Berdufi <adratik.berdufi@gmail.com>
2024-02-07 18:23:43 +0100

    add: first components

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,14 @@
+
+<script></script>
+
+<template> 
+ <h1 class="title">"La mia prima app con Vite!"</h1>
+</template>
+
+<style scoped>
+.title{
+    margin-top: 50px;
+    text-align: center;
+    color: goldenrod;
+}
+  </style>
